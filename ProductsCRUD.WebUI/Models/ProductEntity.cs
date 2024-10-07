8570ad1a9593be965de8985d70309219d6dde7df
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-07 20:40:55 -0300

    chore: base product entity

---
+++ ProductsCRUD.WebUI/Models/ProductEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsCRUD.WebUI.Models
+{
+    [Table("TBL_PRODUTOS")]
+    public class ProductEntity
+    {
+        [Key]
+        [Required]
+        [Column("ID_PRODUTO")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("TX_NOME")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Column("TX_PRECO")]
+        public double Price { get; set; }
+
+        [Required]
+        [Column("TX_TIMESTAMP")]
+        public long Timestamp { get; set; }
+    }
+}
