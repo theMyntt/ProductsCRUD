0624680fd6b997c22fb9eafec7a5c5fc50a3e157
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-07 20:42:49 -0300

    chore: mysql db context

---
+++ ProductsCRUD.WebUI/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsCRUD.WebUI.Models;
+
+namespace ProductsCRUD.WebUI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductEntity>()
+                .HasIndex(x => x.Name)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
