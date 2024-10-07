a9cc4678b5f64f21955d54db6e5cd83d290adb84
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-07 20:50:19 -0300

    chore: inject db context

--- ProductsCRUD.WebUI/Program.cs
+++ ProductsCRUD.WebUI/Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.EntityFrameworkCore;
 using ProductsCRUD.WebUI.Components;
+using ProductsCRUD.WebUI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySQL"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySQL")),
+        args => args.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName));
+});
 
 var app = builder.Build();
 
