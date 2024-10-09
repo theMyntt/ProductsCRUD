9a4c843f96aaa4e842121bb406fa85bbd90bdc72
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-09 07:58:58 -0300

    feat: paginator on end of page

--- ProductsCRUD.WebUI/Components/Pages/Products/FindAll.razor.cs
+++ ProductsCRUD.WebUI/Components/Pages/Products/FindAll.razor.cs
@@ -10,9 +10,25 @@ namespace ProductsCRUD.WebUI.Components.Pages.Products
         [Parameter]
         public int Page { get; set; }
 
+        private int TotalPages { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             products = await _service.GetProductsAsync(Page);
+            var totalItems = await _service.CountItemsAsync();
+            TotalPages = (int)Math.Ceiling((double)totalItems / 10);
+        }
+
+        private List<int> MakePaginator()
+        {
+            var paginator = new List<int>();
+
+            for (int i = 1; i <= TotalPages; i++)
+            {
+                paginator.Add(i);
+            }
+
+            return paginator;
         }
     }
 }
