b4b804bf5aad1155920e5e5b634893fb38b5c305
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-09 07:26:42 -0300

    chore: implement search by page

---
+++ ProductsCRUD.WebUI/Components/Pages/Products/FindAll.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProductsCRUD.WebUI.Models;
+
+namespace ProductsCRUD.WebUI.Components.Pages.Products
+{
+    public partial class FindAll
+    {
+        private IEnumerable<ProductEntity>? products { get; set; }
+
+        [Parameter]
+        public int Page { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            products = await _service.GetProductsAsync(Page);
+        }
+    }
+}
